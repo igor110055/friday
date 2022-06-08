7708cdb91712feead101f6cc5bffcedbb251700a
Kenny PC <kennychavez.mtl@gmail.com>
2022-06-08 17:46:28 -0400

    added files to the git

---
+++ defs.py
@@ -0,0 +1,7 @@
+API_KEY = "647d614e645f5e9baf91dc73fc596580-a5bb88b608640df41b0d857ad569acd8"
+ACCOUNT_ID = "101-002-22473635-001"
+OANDA_URL = 'https://api-fxpractice.oanda.com/v3'
+
+SECURE_HEADER = {
+    'Authorization': f'Bearer {API_KEY}'
+}
---
+++ plot.py
@@ -0,0 +1,4 @@
+import plotly.graph_objects as go
+
+import pandas as pd
+from datetime import datetime
